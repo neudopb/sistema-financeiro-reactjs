1699655d5a108d0549da7348b0d22b179b8914dc
neudinho11 <neudinho66@gmail.com>
2021-10-08 15:53:37 -0300

    Inicio estilização da tela e listagem

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.15},
+    {date: new Date(2021, 9, 15), category: 'food', title: 'Burguer King', value: 28},
+    {date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 450},
+    {date: new Date(2021, 10, 20), category: 'salary', title: 'Salario', value: 3500},
+];
